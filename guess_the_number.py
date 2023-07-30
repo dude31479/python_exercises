ebac35cb642c2aef42f82200a508f122932be78c
Chris Evans <122709941+dude31479@users.noreply.github.com>
2023-07-30 07:30:41 -0400

    Finished guess num

---
+++ guess_the_number.py
@@ -0,0 +1,19 @@
+import random
+
+print("I am thinking of a number between 1 and 20.")
+num = random.randint(1, 20)
+for guesses in range(7):
+    print("Take a guess")
+    guess = int(input())
+
+    if guess < num:
+        print("Too low")
+    elif guess > num:
+        print("Too high")
+    else:
+        break
+
+if guess == num:
+    print("Good job! You guessed the number in {} attempts!".format(guesses))
+else:
+    print("Nope. The number I was thinking of was %s." % (num))
