a8f2e8b5e816639bb6e26981301a4ad230dce9e3
Chris Evans <122709941+dude31479@users.noreply.github.com>
2023-08-10 18:03:06 -0400

    Add comments

--- commaFormat.py
+++ commaFormat.py
@@ -1,21 +1,21 @@
 def commaFromat(num):
     num = str(num)
-    if '.' in num:
-        fractionalPart = num[num.index('.'):]
-        num = num[:num.index('.')]
+    if '.' in num: # If the number is a decimal
+        fractionalPart = num[num.index('.'):] # Save the part after and incl the decimal
+        num = num[:num.index('.')] # Change num to everything up to but not the decimal
     else:
-        fractionalPart = ''
+        fractionalPart = '' # Otherwise, do nothing
 
-    triplet = ''
-    commaNum = ''
+    triplet = '' # Create a temp variable to store digits
+    commaNum = '' # And a second variable to format triplets with commas
 
-    for i in range(len(num) - 1, -1, -1):
-        triplet = num[i] + triplet
-        if len(triplet) == 3:
-            commaNum = triplet + ',' + commaNum
-            triplet = ''
-    if triplet != '':
-        commaNum = triplet + ',' + commaNum
-    return commaNum[:-1] + fractionalPart
+    for i in range(len(num) - 1, -1, -1): # Iterate backwards through the number string
+        triplet = num[i] + triplet # Add the next digit to the left, to the left of triplet 
+        if len(triplet) == 3: # ...until triplet has 3 digits
+            commaNum = triplet + ',' + commaNum # Concatenate triplet with a comma + commaNum
+            triplet = '' # Result the temp triplet variable to store the next 3 digits
+    if triplet != '': # If there are two or one digits left in num
+        commaNum = triplet + ',' + commaNum # Add them to the start of commaNum
+    return commaNum[:-1] + fractionalPart # Get rid of trailing comma and tack on the decimal
 
 print(commaFromat(4423445.345))
